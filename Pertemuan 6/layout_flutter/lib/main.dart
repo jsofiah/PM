b3da3fd27a10bfddc79e8921a659677579675452
jsofiah <j.sofiah18@gmail.com>
2026-04-22 21:12:16 +0700

    Praktikum 2

--- Pertemuan 6/layout_flutter/lib/main.dart
+++ Pertemuan 6/layout_flutter/lib/main.dart
@@ -42,6 +42,18 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
+
+    Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(color, Icons.call, 'CALL'),
+        _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+        _buildButtonColumn(color, Icons.share, 'SHARE'),
+      ],
+    );
+
+
     return MaterialApp(
       title: 'Flutter layout: Sofiah - 244107060065',
       home: Scaffold(
@@ -51,9 +63,34 @@ class MyApp extends StatelessWidget {
         body: Column(
           children: [
             titleSection,
+            buttonSection,
           ],
         ),
       ),
     );
+
   }
+
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
 }
