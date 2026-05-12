d3d07dcf1edd5d776be1026928565c206a19a67c
jsofiah <j.sofiah18@gmail.com>
2026-05-12 09:47:00 +0700

    Langkah 4

--- Pertemuan 7/kamera_flutter/lib/main.dart
+++ Pertemuan 7/kamera_flutter/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'widget/takepicture_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -29,65 +30,8 @@ class MyApp extends StatelessWidget {
           seedColor: Colors.deepPurple,
         ),
       ),
-      home: MyHomePage(
-        title: 'Flutter Demo Home Page',
+      home: TakePictureScreen(
         camera: camera,
-      ),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({
-    super.key,
-    required this.title,
-    required this.camera,
-  });
-
-  final String title;
-  final CameraDescription camera;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor:
-            Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context)
-                  .textTheme
-                  .headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
---
+++ Pertemuan 7/kamera_flutter/lib/widget/takepicture_screen.dart
@@ -0,0 +1,47 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+
+class TakePictureScreen extends StatefulWidget {
+  const TakePictureScreen({
+    super.key,
+    required this.camera,
+  });
+
+  final CameraDescription camera;
+
+  @override
+  State<TakePictureScreen> createState() =>
+      TakePictureScreenState();
+}
+
+class TakePictureScreenState
+    extends State<TakePictureScreen> {
+
+  late CameraController _controller;
+
+  late Future<void> _initializeControllerFuture;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = CameraController(
+      widget.camera,
+      ResolutionPreset.medium,
+    );
+
+    _initializeControllerFuture =
+        _controller.initialize();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
